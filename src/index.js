15d67fc0ca492a57132128fe52404a7b212b79d7
thantd1809 <thanhtd1809@gmail.com>
2024-09-28 10:27:38 +0700

    add backend resource

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Amplify } from "aws-amplify";
-import outputs from "../amplify_outputs.json";
+// import { Amplify } from "aws-amplify";
+// import outputs from "../amplify_outputs.json";
 
-Amplify.configure(outputs)
+// Amplify.configure(outputs)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
